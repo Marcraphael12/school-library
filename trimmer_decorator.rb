c6352eb7b4647ea28018ef5e014bcff5e25101e6
Marc raphael <marcraphael979@gmail.com>
2023-04-22 01:03:14 +0100

    create a method correct_name that trime the output of @nameable.correct_name if it's longer is greate than 10

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,5 +1,9 @@
 require_relative './base_decorator'
 
 class TrimmerDecorator < BaseDecorator
-
+  def correct_name
+    name = @nameable.correct_name
+    name_trimed = "#{name[0...10].strip}..." if name.length > 10
+    name_trimed
+  end
 end
