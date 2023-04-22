872f01f974132e40280943eedc5c48f6d70c7673
Marc raphael <marcraphael979@gmail.com>
2023-04-22 01:00:55 +0100

    Fix typo

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative './nameable'
 
 class Person < Nameable
-  attr_reader :id!
+  attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
