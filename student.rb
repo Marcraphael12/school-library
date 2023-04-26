40cec63dd16f410ebf792a211ea0ad83b549ef2f
Marc raphael <marcraphael979@gmail.com>
2023-04-26 11:39:08 +0100

    Update student object

--- student.rb
+++ student.rb
@@ -15,6 +15,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.student.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
