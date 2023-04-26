ee63779351f781523eed0c035f2ae9a31a9fb4b6
Marc raphael <marcraphael979@gmail.com>
2023-04-26 11:29:20 +0100

    Update add_rental method, Write method to parse a book infos to json format

--- book.rb
+++ book.rb
@@ -1,13 +1,21 @@
 class Book
+  attr_accessor :title, :author, :rentals
+
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
   end
 
-  attr_accessor :title, :author, :rentals
+  def add_rental(date, person)
+    @rentals.push(Rental.new(date, self, person)) unless @rentals.include?(Rental.new(date, self, person))
+  end
 
-  def add_rental(person, date)
-    Rental.new(date, self, person)
+  def to_json(*_args)
+    {
+      'json_class' => self.class.name,
+      'title' => @title,
+      'author' => @author
+    }
   end
 end
