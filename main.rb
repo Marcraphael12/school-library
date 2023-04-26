074d2866544eca1993b341a5eecbdedf67589fa3
Marc raphael <marcraphael979@gmail.com>
2023-04-26 09:05:49 +0100

    remove useless code

--- main.rb
+++ main.rb
@@ -2,11 +2,10 @@ require_relative './app'
 require './data'
 require 'json'
 
-puts 'Welcome to your school library'
 # Takes data from json files
 initialize_files
 
-#Starts the app
+# Starts the app
 def main
   app = App.new
   app.start
