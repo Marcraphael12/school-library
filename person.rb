3dd3a2d081a904b700e33e2c923267cba795d3ad
Marc raphael <marcraphael979@gmail.com>
2023-04-26 11:34:19 +0100

    Push newly created rental into an array

--- person.rb
+++ person.rb
@@ -26,8 +26,8 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(book, date)
-    Rental.new(date, book, self)
+  def add_rental(date, book)
+    @rentals.push(Rental.new(date, book, self)) unless @rentals.include?(Rental.new(date, book, self))
   end
 
   # private method
