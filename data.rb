47117759479712ca8182beed125264bba8407a52
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-26 09:11:12 +0100

    Update data.rb file

--- data.rb
+++ data.rb
@@ -10,25 +10,25 @@ def initialize_files
   File.write('./data/rental_file.json', []) unless File.exist?('./data/rental_file.json')
 end
 
-# def read_books
-#   books = []
-#   JSON.parse(File.read('./data/book_file.json')).each do |book|
-#     books.push(Book.new(book['title'], book['author']))
-#   end
-#   books
-# end
+def read_books
+  books = []
+  JSON.parse(File.read('./data/book_file.json')).each do |book|
+    books.push(Book.new(book['title'], book['author']))
+  end
+  books
+end
 
-# def write_books(books)
-#   temp = []
-#   books.each do |book|
-#     temp.push({
-#                 title: book.title,
-#                 author: book.author
-#               })
-#   end
+def write_books(books)
+  temp = []
+  books.each do |book|
+    temp.push({
+                title: book.title,
+                author: book.author
+              })
+  end
 
-#   File.write('./data/book_file.json', JSON.generate(temp).to_s)
-# end
+  File.write('./data/book_file.json', JSON.generate(temp).to_s)
+end
 
 def write_people(persons)
   # Check weither the person created is a student or a teacher
