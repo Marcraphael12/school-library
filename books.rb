a56b8309aee8262fd21d4479f4a727cdfa22913d
Marc raphael <marcraphael979@gmail.com>
2023-04-26 09:00:23 +0100

    Add comments and fix linters

--- books.rb
+++ books.rb
@@ -3,26 +3,26 @@ require './data'
 require 'json'
 
 class Books
-  #read and write
+  # read and write
   attr_accessor :books
-  
+
   def initialize
     # @books = []
     @books = read_books
   end
-  
+
   def booklist
-    #case there's no book
+    # case there's no book
     puts 'You don\'t have any book yet! Go to the dashboard and add one please.' if @books.empty?
     @books.each { |book| puts "Title📖: #{book.title}, Author👨‍💼: #{book.author}" }
   end
 
-  #create a new book
+  # create a new book
   def create_book
     print 'Enter the Book title please: '
     title = gets.chomp
 
-    print'Nice 🙂'
+    print 'Nice 🙂'
     print 'Now enter the Author name: '
     author = gets.chomp
 
